cf878798a53238ba9aedade122ac4d7d29cee8ec
Debsubhra Roy <debsubhra.roy@gmail.com>
2014-02-13 13:46:20 -0800

    Step 5

--- b.py
+++ b.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python
+import re
 
 line = raw_input("input numbers : ")
 #line = line.rstrip()
@@ -9,10 +10,27 @@ line = line.replace(' ','')
 line = line.replace(';',',')
 vals = line.split(',')
 total = 0
+no_flag = False
 if bool(vals[0]):
 	for val in vals:
-		total += int(val) 
+		match = re.search("^-{0,1}\d+$", val)
+		if bool(match) == False:
+			print "Only numbers can be calulated"
+			break
+		try:
+			if int(val) < 0:
+                                no_flag = True
+				raise ;
+		except :
+			print "negatives not allowed " + val
+			
+		if no_flag == False:
+			total += int(val) 
 	else:
-		print "Total = %d" % total
+		if no_flag == False:
+			print "Total = %d" % total
+		else:
+			# print "Total = %d" % total
+			pass
 else:
 	print "No numbers supplied!"
